4b07954a4fd0d3858afb0605719d658668a6c194
giraffe0000 <longneck_giraffe000@yahoo.ne.jp>
2019-10-31 20:31:55 +0900

    update

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,11 +2,12 @@ $(function() {
   function buildHTML(message) {
 
       //htmlに書き換え  
+      // var addImage = '';
+      // if (message.image.url) {
+      //   addImage = `<img src="${message.image.url}" class="lower-message__image">`;
+      // }
       var addImage = '';
-      if (message.image.url) {
-        addImage = `<img src="${message.image.url}" class="lower-message__image">`;
-      }
-    // let image = message.image? `<img src="${message.image}">` : ""
+      message.image.url ? addImage = `<img src="${message.image.url}" class="lower-message__image">`:"";
 
     let html = `<div class="message" data-message-id=${message.id}>
                   <div class="upper-message">
@@ -54,43 +55,14 @@ $(function() {
         alert('error');
     });
   });
-  // var reloadMessages = function () {
-  //   if (window.location.href.match(/\/groups\/\d+\/messages/)){
-  //     var last_message_id = $('.message:last').data("message-id"); 
-      
-
-  //     $.ajax({ 
-  //       url: "api/messages", 
-  //       type: 'GET', 
-  //       dataType: 'json', 
-  //       data: {id: last_message_id} 
-  //     })
-  //     .done(function (messages) { 
-  //       var insertHTML = '';
-  //       messages.forEach(function (message) {
-  //         insertHTML = buildHTML(message); 
-  //         $('.messages').append(insertHTML);
-  //       })
-  //       // $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-  //     })
-  //     .fail(function () {
-  //       alert('自動更新に失敗しました');
-  //     });
-  //   }
-  // };
-  // setInterval(reloadMessages, 5000);
+  
 
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     last_message_id = $('.message:last').data("message-id");
-    console.log(last_message_id);
     $.ajax({
-      //ルーティングで設定した通りのURLを指定
       url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'GET',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {last_id: last_message_id}
     })
     .done(function(messages) {
@@ -103,7 +75,7 @@ $(function() {
 
     })
     .fail(function() {
-      console.log('error');
+      alert('error');
     });
   };
   setInterval(reloadMessages, 5000);
